c0e1ec5b8141bf513977761515e676a9d053eefc
Babak <babakhadady@gmail.com>
2022-12-28 03:48:42 -0800

    finished content

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -2,6 +2,10 @@ import Visible from "../../hooks/Visible";
 import "./style.css"
 import { useRef } from "react";
 import { Fade } from "react-reveal";
+import { Outlet } from "react-router-dom";
+import { HashLink as Link } from 'react-router-hash-link';
+
+import "../../App/App"
 
 const NavBar = () => {
     const ref = useRef();
@@ -11,13 +15,13 @@ const NavBar = () => {
         <section ref={ref} id="transition" className={(shouldShow ? 'visible' : 'notvisible') + ' navbar'}>
             <Fade delay={2000}>
                 <ul className="details" >
-                    <li><a href="">CONTACT</a></li>
-                    <li><a href="downloads/babak hadady.docx" download>RESUME</a></li>
-                    <li><a href="https://www.linkedin.com/in/babak-hadady-50a857201/">LINKEDIN</a></li>
-                    <li><a href="#experience">EXPERIENCES</a></li>
-                    <li><a href="#aboutme">ABOUT ME</a></li>
+                    <li><a  className="nav" href='#experiences'>EXPERIENCES</a></li>
+                    <li><a className="nav" href="downloads/babak hadady.docx" download>RESUME</a></li>
+                    <li><a target="_blank" className="nav" href="https://www.linkedin.com/in/babak-hadady-50a857201/">LINKEDIN</a></li>
+                    {/* <li><a href="#aboutme">ABOUT ME</a></li> */}
                 </ul>
             </Fade>
+            <Outlet></Outlet>
         </section>
     )
 }
